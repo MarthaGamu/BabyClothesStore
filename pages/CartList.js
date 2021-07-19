f5798f785776068e5dee9efa4b78eb7a6fb5e805
MarthaGamu <marthagmandizvidza60@gmail.com>
2021-07-19 13:31:41 +0100

    cartlist header fixed

--- pages/CartList.js
+++ pages/CartList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Cart from '../components/Cart/Cart';
+import styled from 'styled-components';
+
+const HeroImage = styled.div`
+  width: 100vw;
+  height: 12vh;
+  background-color: #23232c;
+`;
 
 const CartList = () => {
   return (
-    <div>
-      <Cart />
-    </div>
+    <>
+      <HeroImage>{/* <img src={darkmode} alt="dark mode" /> */}</HeroImage>
+      <div>
+        <Cart />
+      </div>
+    </>
   );
 };
 
